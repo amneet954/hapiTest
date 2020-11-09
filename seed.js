f5e1ea7b9112095d5aa2cbd3c6353394724025b3
Amneet Sandhu <amneet954@gmail.com>
2020-11-09 18:17:12 -0500

    cleaned up seed file

--- seed.js
+++ seed.js
@@ -4,18 +4,8 @@ const database = require("./api/database/database");
 const { blue, red } = require("chalk");
 const User = require("./api/database/models/Users");
 
-// const UserSample = [
-//   {
-//     email: "asandhu@hapiTest.com",
-//     password: "Password123!",
-//   },
-// ];
-
 const seed = async () => {
   await database.sync({ force: true });
-
-  //this seeds the database
-  // await Promise.all();
 
   console.log(blue("Seeding success!"));
   database.close();
